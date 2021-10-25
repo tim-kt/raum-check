36279515fb97dccf306f476562fbfac1608e3ea6
Tim Ktitarev <tim.ktitarev@gmail.com>
2021-10-25 12:35:37 +0200

    Use discord.py's command system

--- bot.py
+++ bot.py
@@ -1,80 +1,79 @@
 import os
 import raumcheck
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 from datetime import datetime
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+DISCORD_TAG = os.getenv("DISCORD_TAG")
 ENVIRONMENT = os.getenv("ENVIRONMENT")
 
-client = discord.Client()
+bot = commands.Bot(command_prefix=DISCORD_TAG + " ")
 
-@client.event
+@bot.event
 async def on_ready():
-    print("{} is running".format(client.user.name))
+    print("{} is running".format(bot.user.name))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+@bot.event
+async def on_command_error(ctx, error):
+    print("Error ({}) on command: {}".format(error, ctx.message.content))
+
+@bot.command()
+async def find(ctx, query, date_str=None):
+    """Finds free rooms near the given query string (based on levenshtein distance)."""
+    print("{}: {}".format(ctx.message.author, ctx.message.content))
+    query = query.upper()
+    async with ctx.typing():
+        date = get_date(date_str)
+        rooms = raumcheck.get_sorted_rooms(query)
+        free_rooms = {}
+        for room in rooms:
+            events = raumcheck.get_availability(room, date)
+            if len(events) == 0:
+                free_rooms[room] = events
+                if len(free_rooms) == 5:
+                    break
+        
+    if len(free_rooms) == 0:
+        description = "Ich konnte keine freien Räume finden :("
+    else:
+        description = "Ich habe folgende Räume in deiner Nähe gefunden, die heute nicht belegt sind:\n- **{}**".format("**\n- **".join(free_rooms.keys()))
+        
+    embed = discord.Embed(description=description)
+    embed.set_footer(text="Angaben ohne Gewähr." if ENVIRONMENT == "PRODUCTION" else "Development build.")
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def check(ctx, room, date_str=None):
+    """Checks the given room for availability (today or on the given date)."""
+    print("{}: {}".format(ctx.message.author, ctx.message.content))
+    room = room.upper()
+    date = get_date(date_str)
+    if date is None:
+        # TODO better error handling
+        await ctx.send(embed=discord.Embed(description="Ungültiges Datum angegeben. Bitte nutze das Format DD.MM.YY"))
         return
 
-    if client.user.mentioned_in(message) and message.mention_everyone is False:
-        print("{}: {}".format(message.author, message.content))
-        if "find" in message.content:
-            # A find query should have the following format:
-            # @<mention> find <string> <optionally: number of results>
-            async with message.channel.typing():
-                date = get_date()
-                query = message.content.split()
-                rooms = raumcheck.get_sorted_rooms(query[2].upper())
-                free_rooms = {}
-                for room in rooms:
-                    events = raumcheck.get_availability(room, date)
-                    if len(events) == 0:
-                        free_rooms[room] = events
-                        if len(free_rooms) == int(query[3]) if len(query) > 3 else 5:
-                            break
-            
-            if len(free_rooms) == 0:
-                description = "Ich konnte keine freien Räume finden :("
-            else:
-                description = "Ich habe folgende Räume in deiner Nähe gefunden, die heute nicht belegt sind:\n- **{}**".format("**\n- **".join(free_rooms.keys()))
-            embed = discord.Embed(description=description)
-            await message.channel.send(embed=embed)
+    date_display = "heute" if date_str is None else "am {}".format(date)
+
+    async with ctx.typing():
+        events = raumcheck.get_availability(room, date)
+
+        if events is None:
+            embed = discord.Embed(description="Der Raum **{}** konnte nicht im Raumplan gefunden werden.".format(room))
+        elif len(events) == 0:
+            embed = discord.Embed(description="Der Raum **{}** ist {} nicht belegt.".format(room, date_display))
         else:
-            # A check query should have the following format:
-            # @<mention> <room> <optionally: date>
-            query = message.content.split()
-            if len(query) < 2:
-                await message.channel.send(embed=discord.Embed(description="Bitte gib einen Raum an."))
-                return
+            embed = discord.Embed(description="Der Raum **{}** ist {} zu folgenden Uhrzeiten belegt:".format(room, date_display))
+            for event in events:
+                embed.add_field(name="{} bis {}".format(event["start"], event["end"]), value=event["name"], inline=False)
 
-            room = query[1]
-            date = get_date(date=query[2] if len(query) > 2 else None)
-            if date is None:
-                await message.channel.send(embed=discord.Embed(description="Ungültiges Datum angegeben. Bitte nutze das Format DD.MM.YY"))
-                return
+    embed.set_footer(text="Angaben ohne Gewähr." if ENVIRONMENT == "PRODUCTION" else "Development build.")
+    await ctx.send(embed=embed)
 
-            date_display = "am {}".format(date) if len(query) > 2 else "heute"
-
-            async with message.channel.typing():
-                events = raumcheck.get_availability(room, date)
-
-                if events is None:
-                    embed = discord.Embed(description="Der Raum **{}** konnte nicht im Raumplan gefunden werden.".format(room.upper()))
-                elif len(events) == 0:
-                    embed = discord.Embed(description="Der Raum **{}** ist {} nicht belegt.".format(room.upper(), date_display))
-                else:
-                    embed = discord.Embed(description="Der Raum **{}** ist {} zu folgenden Uhrzeiten belegt:".format(room.upper(), date_display))
-                    for event in events:
-                        embed.add_field(name="{} bis {}".format(event["start"], event["end"]), value=event["name"], inline=False)
-
-            embed.set_footer(text="Angaben ohne Gewähr." if ENVIRONMENT == "PRODUCTION" else "Development build.")
-            await message.channel.send(embed=embed)
-
-
-def get_date(date=None):
+def get_date(date_str):
     """
     Returns one of the following:
     - today's date if the given date is None (format: DD.MM.YY)
@@ -82,12 +81,12 @@ def get_date(date=None):
 
     If the given date does not have three parts separated with a dot, None is returned. 
     """
-    if date is None:
-        date = date = datetime.today().strftime('%d.%m.%Y')
+    if date_str is None:
+        date = datetime.today().strftime('%d.%m.%Y')
         # Convert from DD.MM.YYYY to DD.MM.YY
         date = date[:-4] + date[-2:]
     else:
-        parts = date.split(".")
+        parts = date_str.split(".")
         if len(parts) != 3:
             return None
 
@@ -103,5 +102,4 @@ def get_date(date=None):
 
     return date
 
-
-client.run(TOKEN)
+bot.run(TOKEN)
